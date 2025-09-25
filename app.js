4bbaac4bbadedbe340dbcafcccedbecbe33b4920
unexCoder <unexCoder@unexCoder.local>
2025-09-25 19:33:28 -0300

    upgrade files

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('javascript running');
+
+console.log(document.getElementById('modal'))
+
+
+function closeModal() {
+    console.log('closing modal');
+    document.getElementById('modal').style.display = 'none';
+}
